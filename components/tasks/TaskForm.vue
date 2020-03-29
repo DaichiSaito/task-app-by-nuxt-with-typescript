92702560953e7604cdd65459297b1aeb65d08111
DaichiSaito <hrkedz@gmail.com>
2020-03-29 17:34:30 +0900

    [add]タスクの追加機能

---
+++ components/tasks/TaskForm.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-dialog v-model="isVisible" max-width="500px">
+    <v-card>
+      <v-card-title>
+        <span class="headline">新規追加</span>
+      </v-card-title>
+      <v-card-text>
+        <v-container>
+          <v-row>
+            <v-col cols="12">
+              <v-text-field v-model="task.title" label="タイトル" />
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer />
+        <v-btn @click="close">閉じる</v-btn>
+        <v-btn class="primary" @click="createTask">追加する</v-btn>
+        <v-spacer />
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script lang="ts">
+import { Component, Emit, Prop, Vue } from 'vue-property-decorator';
+import Task from '@/models/task'
+
+@Component
+export default class TaskForm extends Vue {
+  task = {};
+  isVisible = false
+  open() {
+    this.isVisible = true
+  };
+  close() {
+    this.isVisible = false
+  };
+  @Emit("createTask")
+  createTask(): Object {
+    return this.task
+  };
+}
+</script>
--- pages/tasks/index.vue
+++ pages/tasks/index.vue
@@ -1,5 +1,13 @@
 <template>
   <div>
+    <template>
+      <v-btn fab small color="dark" class="mb-2 ml-2" @click="openAddTaskModal">
+        <v-icon>
+          mdi-plus
+        </v-icon>
+      </v-btn>
+    </template>
+    <task-form ref="addTaskModal" @createTask="createTask"></task-form>
     <div v-for="task in tasks" :key="task.id" class="mb-3">
       <task-item :task="task"></task-item>
     </div>
@@ -7,15 +15,27 @@
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator'
+import { Component, Ref, Vue } from 'vue-property-decorator'
 import TaskItem from '@/components/tasks/TaskItem.vue'
+import TaskForm from '@/components/tasks/TaskForm.vue'
 import Task from '@/models/task'
 @Component({
   components: {
-    TaskItem
+    TaskItem,
+    TaskForm
   }
 })
 export default class TaskIndex extends Vue {
-  tasks: Array<Task> = [new Task(1, "title1"), new Task(2, "title2"), new Task(3, "title3")]
+  tasks: Array<Task> = [new Task(1, "title1"), new Task(2, "title2"), new Task(3, "title3")];
+  dialog: Boolean = false;
+  @Ref() addTaskModal: TaskForm;
+  openAddTaskModal() {
+    this.addTaskModal.open()
+  };
+  createTask(task: Object) {
+    const newTask = new Task(this.tasks.length + 1, task.title)
+    this.tasks.push(newTask)
+    this.addTaskModal.close()
+  }
 }
 </script>
